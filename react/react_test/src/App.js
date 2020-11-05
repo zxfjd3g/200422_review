ebc64c179014dd02883c5f68c1696b942207ec06
zxfjd3g <258147149@qq.com>
2020-11-05 10:08:47 +0800

    react

--- react/react_test/src/App.js
+++ react/react_test/src/App.js
@@ -1,16 +1,39 @@
 import React, {Component} from 'react'
+import PropTest from './components/PropTest'
 
-class App extends Component {
+class App2 extends Component {
 
-  render() {
+  state = {
+    count: 0
+  }
+
+  incrementCount = (num) => {
+    this.setState({
+      count: this.state.count + num
+    })
+  }
+
+
+  render () {
+    const {count} = this.state
     return (
       <div>
-        <h2>App组件</h2>
+        <h2>App: {this.state.count}</h2>
+        <button onClick={() => {
+          this.setState({
+            count: this.state.count + 1
+          })
+        }}>更新</button>
 
+        <hr/>
+
+        <PropTest count={count} incrementCount={this.incrementCount}/>
       </div>
     )
   }
 }
 
 
-export default App
+
+// export default App
+export default App2
